a05f0f47aad23070a10ed642195aaff907e7bd52
NyxAether <contact.nyxhemera@gmail.com>
2023-06-05 19:03:40 +0200

    Premier exemple simple

---
+++ python_tkinter_formation/main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from first_example import FeetToMeters
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('Feet to Meters')
+    FeetToMeters(root, padding="3 3 12 12")
+    root.mainloop()
