e1353db089a5f44d91f318bb228e873f9400e625
NyxAether <contact.nyxhemera@gmail.com>
2023-06-07 17:33:51 +0200

    Ajout demo canvas tkinter

--- python_tkinter_formation/main.py
+++ python_tkinter_formation/main.py
@@ -1,8 +1,13 @@
 import tkinter as tk
-from first_example import FeetToMeters
+
+# from first_example import FeetToMeters
+from python_tkinter_formation.bouncing import BouncingCanvas
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title('Feet to Meters')
-    FeetToMeters(root, padding="3 3 12 12")
+    # root.title('Feet to Meters')
+    # FeetToMeters(root, padding="3 3 12 12")
+    root.title("Bouncing balls")
+    root.geometry("500x500")
+    BouncingCanvas(root, bg="black", width=850, height=400)
     root.mainloop()
